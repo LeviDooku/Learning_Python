15d868d8672739a1a80ff494e70f699ac1f59673
LeviDooku <pedrovelascosantana@gmail.com>
2024-05-27 09:19:19 +0200

    nivel_1

--- nivel_1/excepciones_1.py
+++ nivel_1/excepciones_1.py
@@ -6,6 +6,8 @@
 
 #Lanzar varias excepciones en un programa, claúsula finally
 
+#Se pueden concatenar varias excepciones en un mismo bloque try - except. La claúsula finally siempre se ejecuta, útil cuando existen conexiones con BD, fichero etc.
+
 #Las excepciones se pueden consultar: https://docs.python.org/es/3/tutorial/errors.html
 
 def divide(num1, num2):
@@ -13,6 +15,8 @@ def divide(num1, num2):
         return num1 / num2
     except ZeroDivisionError:
         print("[-] ERROR: no se puede dividir por 0") 
+    finally:
+        print("Función terminada")
 
 while True: #Bucle que asegura que se introducen los valores correctos
     try:
@@ -23,3 +27,4 @@ while True: #Bucle que asegura que se introducen los valores correctos
         print("[-] ERROR: tipo de dato incorrecto")
 
 print(f"El resultado de la operación es: {divide(num1, num2)}")
+
