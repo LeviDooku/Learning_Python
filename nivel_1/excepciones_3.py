00da7c5599ca21372bc8bc699d4636204fc7e891
LeviDooku <pedrovelascosantana@gmail.com>
2024-05-28 09:10:47 +0200

    nuevo programa de excepciones personalizadas

--- nivel_1/excepciones_3.py
+++ nivel_1/excepciones_3.py
@@ -14,22 +14,22 @@ class ExcepcionPersonalizada(Exception):
     #Constructor de la clase
     def __init__(self, mensaje = None):
         if mensaje is None:
-            mensaje = "Esto es una excepción personalizada"
+            mensaje = "Esto es una excepción personalizada, y este es su mensaje por defecto"
         self.mensaje = mensaje
         super().__init__(mensaje)
 
-#Función para definir 
+#Función para definir cuando se lanzará la excepción creada
 def edad_valida(edad):
     if edad < 0 or edad > 120:
         raise ExcepcionPersonalizada("[-] ERROR: Edad ingresada inválida")
     return "Edad validada correctamente"
 
-while True: 
+while True: #Bucle infinito hasta ingresar una edad adecuada
     try:
         edad = int(input("Introduzca su edad: "))
-        edad_valida(edad)
+        edad_valida(edad) #Se evalua la edad ingresada
         break
-    except ExcepcionPersonalizada as e:
+    except ExcepcionPersonalizada as e: #"as e" captura una instancia de la excepción y permite acceder a sus atributos
         print(e.mensaje)
 
 print(f"Edad ingresada: {edad}")
